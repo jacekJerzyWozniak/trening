841f6bef3a3ffa82fa41ae0d52a953b2fe745dbc
Jacek Woźniak <jacekjwozniak@gmail.com>
2022-05-30 12:57:53 +0200

    zadanie 1.2 zrobione

--- zadanie_1_z_dnia_21.05.2022.js
+++ zadanie_1_z_dnia_21.05.2022.js
@@ -2,31 +2,54 @@
 //  JUNIOR: wyświetl licznik w konsoli (10,9,8,7....1,koniec!)
 
 
+// let count = 10;
+
+// setInterval(() => {
 
 
-let count = 10;
+//     if (count >= 1) {
+//         console.log(count);
 
+//     }
+//     count--;
 
-
-
-setInterval(() => {
-
-
-    if (count >= 1) {
-        console.log(count);
-    }
-    count--;
-
-}, 500);
-
-
-
+//     if (count == 0) {
+//         setTimeout(() => {
+//             console.log("koniec!");
+//         }, 1000)
+//     }
+// }, 1000);
 
 
 
 
 
 //  REGULAR: wyświetl licznik na stronie (10,9,8,7...1,koniec!)
+
+
+const eleh1 = document.querySelector("h1");
+
+
+
+let count = 10;
+
+setInterval(() => {
+
+
+    if (count >= 1) {
+        eleh1.innerText = count;
+
+    }
+    count--;
+
+    if (count == 0) {
+        setTimeout(() => {
+            eleh1.innerText = "koniec!";
+
+        }, 1000)
+    }
+}, 1000);
+
 
 
 //  SENIOR: startuj licznik za pomocą kliknięcia wprzycisk, kliknięcie ponowne - wyłącza licznik
